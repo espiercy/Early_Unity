dd2acfa18ca46729eea9aab1bec8ac9fbf354093
Evan <evanpiercy@gmail.com>
2018-07-18 17:56:56 -0700

    initial commit

---
+++ Number Wizards/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+
+	int max;
+	int min;
+	int guess;
+	
+	// Use this for initialization
+	void Start () {
+		
+		StartGame ();
+	}
+	
+	void StartGame () {
+		max = 1000;
+		min = 1;
+		guess = 500;
+		
+		print ("=========================");
+		print ("Welcome to Number Wizard");
+		print ("Pick a number in your head but don't tell me!");
+		
+		
+		
+		print ("The highest number you can pick is " + max);
+		print ("The lowest number you can pick is " + min);
+		
+		print ("Is the number higher or lower than " + guess + "?");
+		print ("Up = higher, down = lower, return = equal");
+		
+		max = max + 1;
+	}
+	
+	void NextGuess () {
+		if(Input.GetKeyDown(KeyCode.UpArrow)){
+			//print("Up arrow pressed");
+			min = guess;
+			guess = (max + min) / 2;
+			print ("Higher or lower than " + guess);
+			print ("Up = higher, down = lower, return = equal");
+		}
+		
+		else if(Input.GetKeyDown(KeyCode.DownArrow)){
+			//print("Down arrow pressed");
+			max = guess;
+			guess = (max + min) / 2;
+			print ("Is the number higher or lower than " + guess + "?");
+			print ("Up = higher, down = lower, return = equal");
+			
+		}
+		
+		else if(Input.GetKeyDown(KeyCode.Return)){
+			print("I won!");
+			StartGame();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		NextGuess ();
+	}
+}
---
+++ Number_Wizard_Console/Number Wizard Console/Assets/NumberWizard.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+    private int max, min, guess;
+    private bool found = false;
+
+    // Use this for initialization
+    void Start ()
+    {
+        StartGame();
+    }
+	
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+        WriteWelcome();
+        max++;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        NextGuess();
+	}
+
+    //we can abstract the KeyPress events above through this method? Maybe not how I've constructed this
+    void NextGuess()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            DirectionArrowPressed("higher", changedLimit: ref min);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            DirectionArrowPressed("lower", changedLimit: ref max);
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            FoundSolution();
+        }
+    }
+
+    void DirectionArrowPressed(string keyStr, ref int changedLimit)
+    {
+        Debug.Log("So! It was " + keyStr + " than " + guess);
+        changedLimit = guess;
+        guess = (max + min) / 2;
+        WriteInstructions();
+    }
+
+    void FoundSolution() {
+        Debug.Log("I got it! Yay! I'm a total genuis!");
+        StartGame();
+    }
+
+    void WriteWelcome()
+    {
+        Debug.Log("Tubular Times, Dude! Welcome to the Number Wizard, bro!");
+        Debug.Log("Pick a number!");
+        Debug.Log("The highest number you can pick is: " + max);
+        Debug.Log("The lowest number you can pick is: " + min);
+
+        WriteInstructions();
+    }
+
+    void WriteInstructions() {
+        Debug.Log("Higher or lower than: " + guess);
+        Debug.Log("Push Up Arrow key for higher, Push Down Arrow key for lower, and the Enter key for Correct");
+    }
+
+}
